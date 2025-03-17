d0a402efa3a82e361624d6583d4d3efe0459e18d
SUNGJIN <pxp13716@gmail.com>
2025-03-17 13:56:54 +0900

    .

--- chap01_javascript/src/A04ExportOne.js
+++ chap01_javascript/src/A04ExportOne.js
@@ -1,12 +1,16 @@
 // ESM - ECMAScript Module
 
 // 개별 Export
-const name = '놀부';
-const age = 30;
-const check = true;
+export const name = '놀부';
+export const age = 30;
+export const check = true;
+
 const arr = [10, 20];
 const user = { name: '흥부', age: 20 };
 const onAdd = (x, y) => `${x} + ${y} = ${x + y}`;
 
 // 묶어서 개별 Export
+// as로 export 이름을 변경할 수 있다
+export { arr, user, onAdd as add };
 
+console.log(`Export ONE - ${name} / ${arr[0]} / ${user.name} / ${onAdd(1, 2)}`);
